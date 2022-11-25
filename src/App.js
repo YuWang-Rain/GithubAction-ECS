ea838bf2d49a878e3e7c6fe6ee094607787ae617
wangyu.fish <wangyu.fish@bytedance.com>
2022-11-25 14:26:12 +0800

    demo for github action

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!!
+          demo for github action!
         </a>
       </header>
     </div>
